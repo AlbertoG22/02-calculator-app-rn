351f86eadcc07f78f60112b2d8077e508c6b0077
AlbertoG22 <albertogarciamartinez99@gmail.com>
2025-06-18 20:48:28 -0600

    buttons and styles

--- src/presentation/screens/CalculatorScreen.tsx
+++ src/presentation/screens/CalculatorScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Pressable, Text, View } from 'react-native';
 import { styles } from '../../config/theme/app-theme';
 
 export const CalculatorScreen = () => {
@@ -9,6 +9,12 @@ export const CalculatorScreen = () => {
       <View style={{ paddingHorizontal: 30, paddingBottom: 20 }}>
         <Text style={ styles.mainResult }>1500</Text>
         <Text style={ styles.subResult }>15</Text>
+      </View>
+
+      <View style={ styles.row }>
+        <Pressable style={ styles.button }>
+          <Text style={ styles.buttonText }>0</Text>
+        </Pressable>
       </View>
 
     </View>
